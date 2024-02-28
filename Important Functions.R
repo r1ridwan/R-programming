9ae992162b57a023c88df34a01fadc72b4af2bff
Md. Ridone Ahmed <ridwan.nstu@gmail.com>
2024-02-28 16:46:29 +0600

    ljosghlskjglskdg

--- Important Functions.R
+++ Important Functions.R
@@ -2,7 +2,7 @@
 getwd()
 #Set New Working Directory
 setwd("I:/Bioinformatics/Scripts")
-=========================================
+-----------------------------------------------------------
 
 #If you want to check what things you have done througout the full session at a particular time, you should use 
 sessionInfo()
